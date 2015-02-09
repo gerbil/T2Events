9930ef187581cd1e430cd578eef017eaf4236a08
gerbil <jurijs.kobecs@gmail.com>
2015-02-09 15:25:26 +0200

    changes

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -13,31 +13,24 @@ angular.module('t2EventsApp')
 
         // Today events +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-        var today = Date.today().toFormat('YYYY-MM-DD');
-        var tomorrow = Date.tomorrow().toFormat('YYYY-MM-DD');
+        var today = Date.today().toFormat('YYYY-MM-DDT');
+        var tomorrow = Date.today().toFormat('YYYY-MM-DDT23:59:59Z');
 
         var date = new Date();
         var minutes = date.getMinutes();
         minutes = minutes > 9 ? minutes : '0' + minutes;
         $scope.currentTime = date.getHours() + ':' + minutes;
 
+        today = today + $scope.currentTime + ':00Z';
+
         Restangular.all('calendar').getList({'startDateTime': today, 'endDateTime': tomorrow})
             .then(function (results) {
-                $scope.events = results[0];
                 $scope.nextEvent = results[0].value[0];
 
-                var nextEventStartHours = new Date(Date.parse($scope.nextEvent.Start)).getHours();
-                var nextEventStartMinutes = new Date(Date.parse($scope.nextEvent.Start)).getMinutes();
-                nextEventStartMinutes = nextEventStartMinutes > 9 ? nextEventStartMinutes : '0' + nextEventStartMinutes;
-                $scope.nextEvent.Start = nextEventStartHours + ':' + nextEventStartMinutes;
+                $scope.nextEvent.Start = new Date(Date.parse($scope.nextEvent.Start)).removeHours(1).toFormat('HH24:MI');
+                $scope.nextEvent.End = new Date(Date.parse($scope.nextEvent.End)).removeHours(1).toFormat('HH24:MI');
 
-                var nextEventEndHours = new Date(Date.parse($scope.nextEvent.End)).getHours();
-                var nextEventEndMinutes = new Date(Date.parse($scope.nextEvent.End)).getMinutes();
-                nextEventEndMinutes = nextEventEndMinutes > 9 ? nextEventEndMinutes : '0' + nextEventEndMinutes;
-                $scope.nextEvent.End = nextEventEndHours + ':' + nextEventEndMinutes;
-
-
-                $scope.meetingText = $scope.currentTime <  (nextEventEndHours + ':' + nextEventEndMinutes) ? 'Next meeting' : 'Current meeting';
+                $scope.meetingText = ($scope.nextEvent.Start) > $scope.currentTime ? 'Next meeting' : 'Current meeting';
             });
 
     });
