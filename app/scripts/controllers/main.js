3ba61f8223dc88cfa2ee9d9c89a343087e9fb773
gerbil <jurijs.kobecs@gmail.com>
2015-02-09 15:52:48 +0200

    some comments + refresh

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,29 +9,49 @@
  */
 angular.module('t2EventsApp')
 
-    .controller('MainCtrl', function ($scope, Restangular) {
+    .controller('MainCtrl', function ($scope, Restangular, $interval) {
 
         // Today events +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-        var today = Date.today().toFormat('YYYY-MM-DDT');
-        var tomorrow = Date.today().toFormat('YYYY-MM-DDT23:59:59Z');
+        function refreshData() {
+            // We need current timestamp for display
+            var date = new Date();
+            var minutes = date.getMinutes();
+            minutes = minutes > 9 ? minutes : '0' + minutes;
+            $scope.currentTime = date.getHours() + ':' + minutes;
+            // Create today date string for backend query
+            var today = Date.today().toFormat('YYYY-MM-DDT');
+            // Add leading zeros and Z for timestamp outlook API
+            today = today + $scope.currentTime + ':00Z';
+            // Create tomorrow date string for backend query
+            var tomorrow = Date.today().toFormat('YYYY-MM-DDT23:59:59Z');
 
-        var date = new Date();
-        var minutes = date.getMinutes();
-        minutes = minutes > 9 ? minutes : '0' + minutes;
-        $scope.currentTime = date.getHours() + ':' + minutes;
+            Restangular.all('calendar').getList({'startDateTime': today, 'endDateTime': tomorrow})
+                .then(function (results) {
+                    $scope.nextEvent = results[0].value[0];
 
-        today = today + $scope.currentTime + ':00Z';
+                    $scope.nextEvent.Start = new Date(Date.parse($scope.nextEvent.Start)).removeHours(1).toFormat('HH24:MI');
+                    $scope.nextEvent.End = new Date(Date.parse($scope.nextEvent.End)).removeHours(1).toFormat('HH24:MI');
 
-        Restangular.all('calendar').getList({'startDateTime': today, 'endDateTime': tomorrow})
-            .then(function (results) {
-                $scope.nextEvent = results[0].value[0];
+                    $scope.meetingText = ($scope.nextEvent.Start) > $scope.currentTime ? 'Next meeting' : 'Current meeting';
+                });
+        };
 
-                $scope.nextEvent.Start = new Date(Date.parse($scope.nextEvent.Start)).removeHours(1).toFormat('HH24:MI');
-                $scope.nextEvent.End = new Date(Date.parse($scope.nextEvent.End)).removeHours(1).toFormat('HH24:MI');
+        refreshData();
 
-                $scope.meetingText = ($scope.nextEvent.Start) > $scope.currentTime ? 'Next meeting' : 'Current meeting';
-            });
+        // Promise should be created to be deleted afterwards
+        var promise = $interval(refreshData, 25000);
+
+        // Cancel interval on page changes
+        $scope.$on('$destroy', function () {
+            if (angular.isDefined(promise)) {
+                $interval.cancel(promise);
+                promise = undefined;
+            }
+        });
+        // Data refresh end
+
+
 
     });
 
